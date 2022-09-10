a61d1c2186603ab94868b5349e62ccedd14b3a49
krisv <105808514+krisvers@users.noreply.github.com>
2022-09-09 17:16:23 -0700

    Update rp.c

--- rp.c
+++ rp.c
@@ -1,3 +1,9 @@
+/*
+	Very simple program for copying files
+	
+	krisvers - 9/10/22
+*/
+
 #include <stdio.h>
 
 int main(int argc, char **argv)
@@ -8,16 +14,19 @@ int main(int argc, char **argv)
 
 	if (argc == 3)
 	{
+		// Checks for file_one to exist, then opens both
 		if (fopen(argv[1], "r") != NULL)
 		{
 			file_one = fopen(argv[1], "r");
 			file_two = fopen(argv[2], "w");
 		}
+		// File doesn't exist
 		else {
 			fprintf(stdout, "File %s does not exist or cannot be read.\n",argv[1]);
 			return -1;
 		}
 
+		// Copys until end of file	
 		while (c != EOF)
 		{
 			fprintf(file_two, "%c", c);
@@ -25,6 +34,7 @@ int main(int argc, char **argv)
 		}
 	}
 	else {
+		// Prints help
 		fprintf(stdout, "USAGE: rp <file> <copied file>\n    Bad recreation of cp but smol.\n");
 	}
 
